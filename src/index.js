09a2a440c0b37568dd292707575aad73a91df393
ariadna <serw2203@yandex.ru>
2020-04-14 04:42:26 +0300

    package

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-module.exports.summa = (x, y) => x + y;
+let sum = (x, y) => x + y;
+let sub = (x, y) => x - y;
+
+
+module.exports = {sum, sub};
--- test/test.js
+++ test/test.js
@@ -1,13 +1,20 @@
 const {expect, assert} = require('chai');
-const {summa} = require('../src/index');
+const {sum, sub} = require('../src/index');
 
 describe("Пример тестирования", () => {
 
-    it("Складываем 2 числа: 2 + 3 = 5 - assert ---", () => {
-        assert.equal(summa(2, 3), 5);
+    it("Складываем 2 числа: 2 + 3 = 5 (assert)", () => {
+        let result = sum(2, 3);
+        assert.equal(result, 5);
     });
   
-    it("Складываем 2 числа: 2 + 3 = 5 - expect ---", () => {
-        expect(summa(2, 3)).to.equal(5);
+    it("Складываем 2 числа: 2 + 3 = 5 (expect)", () => {
+        let result = sum(2, 3);
+        expect(result).to.equal(5);
+    });
+
+    it("Вычитаем 2 числа: 2 - 3 = -1 (expect)", () => {
+        let result = sub(2, 3);
+        expect(result).to.equal(-1);
     });
 });
